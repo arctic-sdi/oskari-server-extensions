09629a2e5f9e7884b248e2997c0bccbf9b6c3976
Sami Mäkinen <sami.makinen@nls.fi>
2020-09-08 16:41:20 +0300

    Cleanup migrations for Oskari 2

--- asdi-resources/resources/flyway/asdi/V1_0_4__configure_catalogue_metadataflyout_to_not_show_print_link.sql
+++
@@ -1,3 +0,0 @@
-UPDATE portti_view_bundle_seq
-   SET config='{"hideMetaDataPrintLink":true}'
- WHERE bundle_id=(SELECT id FROM portti_bundle WHERE name='metadataflyout');
---
+++ asdi-resources/src/main/resources/flyway/asdi2/V2_0_0__new_init.sql
@@ -0,0 +1,4 @@
+-- NOTE! This will not initialize content to empty database. An existing database dump is required to setup the app.
+-- There are some init files in this maven module that would result in a close approximation but its not worth to spend
+-- effort creating the empty db setup before we actually need it so this is a placeholder for asdi2 module of migrations.
+SELECT now();
