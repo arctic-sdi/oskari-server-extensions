2a1daf806d87f20213613b0b06a40ef1b0743ce0
Sami Mäkinen <sami.makinen@nls.fi>
2020-09-14 16:51:56 +0300

    Use string version so we dont need import

--- asdi-resources/src/main/java/flyway/asdistats/V1_0__add_statsgrid_to_geoportal.java
+++ asdi-resources/src/main/java/flyway/asdistats/V1_0__add_statsgrid_to_geoportal.java
@@ -1,6 +1,5 @@
 package flyway.asdistats;
 
-import fi.nls.oskari.domain.map.view.Bundle;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 import org.oskari.helpers.AppSetupHelper;
@@ -11,7 +10,6 @@ import org.oskari.helpers.AppSetupHelper;
 public class V1_0__add_statsgrid_to_geoportal extends BaseJavaMigration {
 
     public void migrate(Context context) throws Exception {
-        AppSetupHelper.addBundleToDefaultAndUserApps(
-                context.getConnection(), new Bundle("statsgrid"));
+        AppSetupHelper.addBundleToApps(context.getConnection(), "statsgrid");
     }
 }
