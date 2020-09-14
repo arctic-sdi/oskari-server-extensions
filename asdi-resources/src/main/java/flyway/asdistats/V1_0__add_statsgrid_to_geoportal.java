1194a0dad0bee4fe2f8ef2a2312178309c1ce278
Sami Mäkinen <sami.makinen@nls.fi>
2020-09-14 16:02:46 +0300

    Refactor bundle addition

--- asdi-resources/src/main/java/flyway/asdistats/V1_0__add_statsgrid_to_geoportal.java
+++ asdi-resources/src/main/java/flyway/asdistats/V1_0__add_statsgrid_to_geoportal.java
@@ -1,26 +1,17 @@
 package flyway.asdistats;
 
+import fi.nls.oskari.domain.map.view.Bundle;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 import org.oskari.helpers.AppSetupHelper;
-
-import java.sql.Connection;
-import java.util.List;
 
 /**
  * Add statsgrid bundle to views that don't have it.
  */
 public class V1_0__add_statsgrid_to_geoportal extends BaseJavaMigration {
-    private static final String BUNDLE_ID = "statsgrid";
 
     public void migrate(Context context) throws Exception {
-        Connection connection = context.getConnection();
-        final List<Long> views = AppSetupHelper.getSetupsForUserAndDefaultType(connection);
-        for (Long viewId : views) {
-            if (AppSetupHelper.appContainsBundle(connection, viewId, BUNDLE_ID)) {
-                continue;
-            }
-            AppSetupHelper.addBundleToApp(connection, viewId, BUNDLE_ID);
-        }
+        AppSetupHelper.addBundleToDefaultAndUserApps(
+                context.getConnection(), new Bundle("statsgrid"));
     }
 }
