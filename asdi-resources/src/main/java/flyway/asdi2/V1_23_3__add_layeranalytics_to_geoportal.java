9e18aeaa6eee50c184653450583db4f904113dfd
Sami Mäkinen <sami.makinen@nls.fi>
2022-04-25 14:49:02 +0300

    1.23.0 with Oskari 2.7.1

--- asdi-resources/src/main/java/flyway/asdi2/V1_23_3__add_layeranalytics_to_geoportal.java
+++ asdi-resources/src/main/java/flyway/asdi2/V1_23_3__add_layeranalytics_to_geoportal.java
@@ -5,11 +5,11 @@ import org.flywaydb.core.api.migration.Context;
 import org.oskari.helpers.AppSetupHelper;
 
 /**
- * Add layerswipe bundle to views that don't have it.
+ * Add layeranalytics bundle to views that don't have it.
  */
-public class V1_23_2__add_layerswipe_to_geoportal extends BaseJavaMigration {
+public class V1_23_3__add_layeranalytics_to_geoportal extends BaseJavaMigration {
 
     public void migrate(Context context) throws Exception {
-        AppSetupHelper.addBundleToApps(context.getConnection(), "layerswipe");
+        AppSetupHelper.addBundleToApps(context.getConnection(), "layeranalytics");
     }
 }
